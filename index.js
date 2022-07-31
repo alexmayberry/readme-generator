e6d3afaa82f010aa3b98b2c63ef386e59b67c58c
alexmayberry <alexclimbs@gmail.com>
2022-07-30 21:41:17 -0700

    added initial files and basic prompt script

---
+++ index.js
@@ -0,0 +1,98 @@
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// // Create an array of questions for user input
+// const questions = 
+
+// TODO how to add a console.log before each question to indicate how many questions are remaining?
+
+// inquirer
+// .prompt([
+//     // TODO y/n promt for table of contents
+//     {
+//     type: 'input',
+//     message: 'Add usage instructions',
+//     name: 'usage',
+//     },
+//     {
+//     type: 'input',
+//     message: 'What is your name?',
+//     name: 'author',
+//     },
+//     {
+//     type: 'input',
+//     message: 'Add author email',
+//     name: 'usage',
+//     },
+//     {
+//     type: 'input',
+//     message: 'What is your github.com username?',
+//     name: 'github',
+//     },
+//     // TODO Add a y/n prompt to determine if there are multiple authors
+//     {
+//     type: 'input',
+//     message: 'What is the project name',
+//     name: 'title',
+//     },
+//     {
+//       type: 'input',
+//       message: 'Add project description',
+//       name: 'description',
+//     },  
+//     {
+//     type: 'input',
+//     message: 'Add the project repo URL',
+//     name: 'repo',
+//     },
+//     {
+//     type: 'input',
+//     message: 'Add the project live link',
+//     name: 'live',
+//     },
+//     {
+//     type: 'input',
+//     message: 'Add installation instructions',
+//     name: 'installation',
+//     },
+//     {
+//     type: 'input',
+//     message: 'Add usage instructions',
+//     name: 'usage',
+//     },
+//     {
+//     type: 'input',
+//     message: 'Add testing instructions',
+//     name: 'test',
+//     },
+//     {
+//     type: 'input',
+//     message: 'Add constribution instructions',
+//     name: 'constribution',
+//     },
+//     {
+//     type: 'input',
+//     message: 'Add preview image',
+//     name: 'image',
+//     },
+//     {
+//     type: 'input',
+//     message: 'Add video-walkthrough link',
+//     name: 'walkthrough',
+//     },
+//     {
+//     type: 'input',
+//     message: 'Add instructions for questions and issues. Please use .md syntax for links aka [linked text](link)',
+//     name: 'issues',
+//     },
+// ])
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
