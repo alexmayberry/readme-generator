e21ec2bbb6f033b1f2867c6c0ccc5c3c47c030db
alexmayberry <alexclimbs@gmail.com>
2022-08-01 09:38:58 -0700

    added .gitignore, removed the wrong inquirer version, removed fs, and updated the fs writefile function

--- index.js
+++ index.js
@@ -1,95 +1,103 @@
 // Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 
-// // Create an array of questions for user input
-// const questions = 
+// Create an array of questions for user input
+const questions = 
 
 // TODO how to add a console.log before each question to indicate how many questions are remaining?
 
-// inquirer
-// .prompt([
-//     // TODO y/n promt for table of contents
-//     {
-//     type: 'input',
-//     message: 'Add usage instructions',
-//     name: 'usage',
-//     },
-//     {
-//     type: 'input',
-//     message: 'What is your name?',
-//     name: 'author',
-//     },
-//     {
-//     type: 'input',
-//     message: 'Add author email',
-//     name: 'usage',
-//     },
-//     {
-//     type: 'input',
-//     message: 'What is your github.com username?',
-//     name: 'github',
-//     },
-//     // TODO Add a y/n prompt to determine if there are multiple authors
-//     {
-//     type: 'input',
-//     message: 'What is the project name',
-//     name: 'title',
-//     },
-//     {
-//       type: 'input',
-//       message: 'Add project description',
-//       name: 'description',
-//     },  
-//     {
-//     type: 'input',
-//     message: 'Add the project repo URL',
-//     name: 'repo',
-//     },
-//     {
-//     type: 'input',
-//     message: 'Add the project live link',
-//     name: 'live',
-//     },
-//     {
-//     type: 'input',
-//     message: 'Add installation instructions',
-//     name: 'installation',
-//     },
-//     {
-//     type: 'input',
-//     message: 'Add usage instructions',
-//     name: 'usage',
-//     },
-//     {
-//     type: 'input',
-//     message: 'Add testing instructions',
-//     name: 'test',
-//     },
-//     {
-//     type: 'input',
-//     message: 'Add constribution instructions',
-//     name: 'constribution',
-//     },
-//     {
-//     type: 'input',
-//     message: 'Add preview image',
-//     name: 'image',
-//     },
-//     {
-//     type: 'input',
-//     message: 'Add video-walkthrough link',
-//     name: 'walkthrough',
-//     },
-//     {
-//     type: 'input',
-//     message: 'Add instructions for questions and issues. Please use .md syntax for links aka [linked text](link)',
-//     name: 'issues',
-//     },
-// ])
+inquirer
+.prompt([
+    // TODO y/n promt for table of contents
+    {
+    type: 'input',
+    message: 'What type of licenceis connected to your project',
+    name: 'licence',
+    },
+    {
+    type: 'input',
+    message: 'Add author name',
+    name: 'author',
+    },
+    {
+    type: 'input',
+    message: 'Add author email',
+    name: 'usage',
+    },
+    {
+    type: 'input',
+    message: 'What is your github.com username?',
+    name: 'github',
+    },
+    // TODO Add a y/n prompt to determine if there are multiple authors
+    {
+    type: 'input',
+    message: 'What is the project name',
+    name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Add project description',
+      name: 'description',
+    },  
+    {
+    type: 'input',
+    message: 'Add the project repo URL',
+    name: 'repo',
+    },
+    {
+    type: 'input',
+    message: 'Add the project live link',
+    name: 'live',
+    },
+    {
+    type: 'input',
+    message: 'Add installation instructions',
+    name: 'installation',
+    },
+    {
+    type: 'input',
+    message: 'Add usage instructions',
+    name: 'usage',
+    },
+    {
+    type: 'input',
+    message: 'Add testing instructions',
+    name: 'test',
+    },
+    {
+    type: 'input',
+    message: 'Add constribution instructions',
+    name: 'constribution',
+    },
+    {
+    type: 'input',
+    message: 'Add preview image',
+    name: 'image',
+    },
+    {
+    type: 'input',
+    message: 'Add video-walkthrough link',
+    name: 'walkthrough',
+    },
+    {
+    type: 'input',
+    message: 'Add instructions for questions and issues. Please use .md syntax for links aka [linked text](link)',
+    name: 'issues',
+    },
+])
 
 // // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
+.then((response) => {
+    let mdContent = generateMarkdown(data);
+    fs.writeFile('newREADME.md', mdContent, (err) => 
+  err ? console.log(err) : console.log('Success! A README was created')
+);
+  }
+  );
 
 // // TODO: Create a function to initialize app
 // function init() {}
